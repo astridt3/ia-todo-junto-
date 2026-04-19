9c2f3e44de88348d76b275c2c9e17bcef85ad838
astridt3 <rocio.beck05@gmail.com>
2026-04-19 00:53:41 -0300

    segund ia

---
+++ Assets/IA(FSM)/EnemyControllerFSM.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EnemyControllerFSM : MonoBehaviour
+
+{
+    public Transform player;
+    private LineOfSight los;
+    //private EnemyTree desicionTree;
+    //private EnemyContext context;
+
+    [SerializeField] private float speed = 3;
+    [SerializeField] private float rotationSpeed = 33;
+    [SerializeField] private float patrolRotationSpeed = 33;
+
+    //[SerializeField] private Material attackMaterial;
+    //private Material defaultMaterial;
+    //private MeshRenderer renderer;
+
+    private Rigidbody playerRB;
+    private Vector3 wanderDirection;
+    private float wanderTime;
+    [SerializeField] private float WanderchangeInterval = 1.5f;
+    private Vector3 dir;
+    private bool isAttacking = false;
+    private FSMClasses fsm;
+    private Coroutine freezeRoutine;
+
+
+    private void Awake()
+    {
+        fsm = GetComponent<FSMClasses>();
+        los = GetComponent<LineOfSight>();
+        //desicionTree = GetComponent<EnemyTree>();
+        wanderDirection = transform.forward;
+        //c/ontext = new EnemyContext { self = transform, player = player, los = los };/
+
+        //dir = Vector3.zero;
+        //renderer = GetComponent<MeshRenderer>();
+
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("player").transform;
+
+        //renderer = GetComponent<MeshRenderer>();
+        //defaultMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    public void Update()
+    {
+        bool canSeePlayer = los.IsRange(transform, player) && los.IsObstacle(transform, player);
+        Debug.Log(canSeePlayer);
+        fsm.UpdateState(canSeePlayer);
+
+        Move(dir);
+    }
+    //public void StopAttack()
+    //{
+    //    isAttacking = false;
+    //}
+    public void Pursuit()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        direction.y = 0;
+        Vector3 moveDirection = direction.normalized;
+
+        transform.position += moveDirection * speed * Time.deltaTime;
+
+        transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+    }
+    public void FreezePlayer(bool freeze)
+    {
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = freeze;
+        }
+    }
+
+    public void Patrol()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+        //renderer.material = defaultMaterial;
+    }
+    //public void Attack()
+    //{
+    //    Debug.Log("Empieza a atacar");
+    //    isAttacking = true;
+    //    if (isAttacking)
+    //    {
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //        dir = Vector3.zero;
+    //    }
+    //    //renderer.material = attackMaterial;
+    //    Debug.Log("Deja de atacar");
+    //}
+    public void Wander()
+    {
+        wanderTime -= Time.deltaTime;
+        if (wanderTime <= 0f)
+        {
+            wanderDirection = SteeringBehaviours.Wander(wanderDirection, 180f);
+            wanderTime = WanderchangeInterval;
+        }
+        dir = wanderDirection;
+
+        Debug.Log("vAYA");
+    }
+    public void Seek()
+    {
+        dir = SteeringBehaviours.Seek(transform, player.transform.position);
+
+        Debug.Log("vAYA123");
+    }
+
+    private void Move(Vector3 dir)
+    {
+        transform.position += dir * speed * Time.deltaTime;
+
+        if (dir != Vector3.zero)
+        {
+            transform.forward = Vector3.Lerp(transform.forward, dir, Time.deltaTime * rotationSpeed);
+        }
+    }
+    public void FreezePlayer(float duration)
+    {
+        // evita que se acumulen freezes
+        if (freezeRoutine != null)
+            StopCoroutine(freezeRoutine);
+
+        freezeRoutine = StartCoroutine(FreezeCoroutine(duration));
+    }
+
+    private IEnumerator FreezeCoroutine(float duration)
+    {
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
---
+++ Assets/IA(FSM)/FSMClasses.cs
@@ -0,0 +1,214 @@
+using UnityEngine;
+
+public class FSMClasses : MonoBehaviour
+{
+    State currentState { get; set; }
+
+    private PatrolState patrolState;
+    private PursuitState pursuitState;
+    //private AttackState attackState;
+    public EnemyControllerFSM enemy;
+    private FreezeState freezeState;
+    private SearchState searchState;
+    public State _currentState { get { return currentState; } set { currentState = value; } }
+
+    private void Awake()
+    {
+        enemy = GetComponent<EnemyControllerFSM>();
+        freezeState = new FreezeState(this);
+        patrolState = new PatrolState(this);
+        pursuitState = new PursuitState(this);
+        //attackState = new AttackState(this);
+        searchState = new SearchState(this);
+
+        currentState = patrolState;
+    }
+    public void ChangeToFreeze()
+    {
+        ChangeState(freezeState);
+    }
+    public void ChangeToPatrol()
+    {
+        ChangeState(patrolState);
+    }
+
+    public void ChangeToPursuit()
+    {
+        ChangeState(pursuitState);
+    }
+
+    //public void ChangeToAttack()
+    //{
+    //    ChangeState(attackState);
+    //}
+
+    public void ChangeState(State newState)
+    {
+        if (currentState == newState)
+        {
+            return;
+        }
+
+        currentState.Exit();
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    public void UpdateState(bool canSeePlayer)
+    {
+        currentState.Update(canSeePlayer);
+    }
+    public void ChangeToSearch()
+    {
+        ChangeState(searchState);
+    }
+    public void ToSearch() => ChangeState(searchState);
+
+    public void ToPatrol() => ChangeState(patrolState);
+    public void ToPursuit() => ChangeState(pursuitState);
+    //public void ToAttack() => ChangeState(attackState);
+    public void ToFreeze() => ChangeState(freezeState);
+}
+
+public abstract class State
+{
+    protected FSMClasses fsm;
+    public State(FSMClasses fsm)
+    {
+        this.fsm = fsm;
+    }
+
+    public virtual void Enter() { }
+
+    public virtual void Exit() { }
+
+    public abstract void Update(bool canSeePlayer);
+}
+
+public class PatrolState : State
+{
+    public PatrolState(FSMClasses fsm) : base(fsm) { }
+
+    public override void Update(bool canSeePlayer)
+    {
+        fsm.enemy.Wander();
+
+        if (canSeePlayer)
+        {
+            fsm.ToPursuit();
+        }
+    }
+}
+
+public class PursuitState : State
+{
+    public PursuitState(FSMClasses fsm) : base(fsm) { }
+
+    public override void Update(bool canSeePlayer)
+    {
+        fsm.enemy.Seek();
+
+        float distance = Vector3.Distance(
+            fsm.enemy.transform.position,
+            fsm.enemy.player.position
+        );
+
+        if (!canSeePlayer)
+        {
+            fsm.ToPatrol();
+        }
+        if (!canSeePlayer)
+        {
+            fsm.ToPatrol();
+        }
+        //else if (distance < 2f)
+        //{
+        //    fsm.ToAttack();
+        //}
+        else if (distance <= 4f)
+        {
+            fsm.ToFreeze();
+        }
+    }
+}
+public class FreezeState : State
+{
+    private float freezeTime = 1f;
+    private float timer;
+
+    public FreezeState(FSMClasses fsm) : base(fsm) { }
+
+    public override void Enter()
+    {
+        timer = freezeTime;
+        fsm.enemy.FreezePlayer(freezeTime);
+    }
+
+    public override void Update(bool canSeePlayer)
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            fsm.ToSearch();
+        }
+    }
+}
+public class SearchState : State
+{
+    private float searchTime = 10f;
+    private float timer;
+
+    public SearchState(FSMClasses fsm) : base(fsm) { }
+
+    public override void Enter()
+    {
+        timer = searchTime;
+    }
+
+    public override void Update(bool canSeePlayer)
+    {
+        fsm.enemy.Wander();
+
+        timer -= Time.deltaTime;
+
+        if (canSeePlayer)
+        {
+            fsm.ToPursuit();
+            return;
+        }
+
+        if (timer <= 0f)
+        {
+            fsm.ToPatrol();
+        }
+    }
+}
+
+//public class AttackState : State
+//{
+//    public AttackState(FSMClasses fsm) : base(fsm) { }
+
+//    public override void Enter()
+//    {
+//        fsm.enemy.Attack();
+//    }
+
+//    public override void Exit()
+//    {
+//        fsm.enemy.StopAttack();
+//    }
+
+//    public override void Update(bool canSeePlayer)
+//    {
+//        float distance = Vector3.Distance(
+//            fsm.enemy.transform.position,
+//            fsm.enemy.player.position
+//        );
+
+//        if (distance > 2f)
+//        {
+//            fsm.ToPursuit();
+//        }
+//    }
+//}
