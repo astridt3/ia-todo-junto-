ef01b191a0618a89c2b14dbb8c4f461569658990
astridt3 <rocio.beck05@gmail.com>
2026-06-28 18:36:26 -0300

    .

--- Assets/IA (Juntar Steering con tree)/LineOfSight.cs
+++ Assets/IA (Juntar Steering con tree)/LineOfSight.cs
@@ -34,8 +34,18 @@ public class LineOfSight : MonoBehaviour
 
     public bool IsObstacle(Transform self, Transform target)
     {
-        Vector3 dir = target.position - self.position;
+        BoxCollider box = self.GetComponent<BoxCollider>();
 
-        return Physics.Raycast(self.position, dir.normalized, dir.magnitude, obs);
+        Vector3 dir = (target.position - self.position).normalized;
+        float distance = Vector3.Distance(self.position, target.position);
+
+        return Physics.BoxCast(
+            box.bounds.center,
+            box.bounds.extents,
+            dir,
+            self.rotation,
+            distance,
+            obs
+        );
     }
 }
